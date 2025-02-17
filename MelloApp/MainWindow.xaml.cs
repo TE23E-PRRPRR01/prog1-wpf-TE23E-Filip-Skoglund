c9304aff26fb957e151e9b506ac02450ea96f530
SkogisAB <filip.skoglund@elev.ntig.se>
2025-02-17 13:15:20 +0100

    MelloApp

--- MelloApp/MainWindow.xaml.cs
+++ MelloApp/MainWindow.xaml.cs
@@ -16,8 +16,51 @@ namespace MelloApp;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private int antalRöd = 0;
+    private int antalBlå = 0;
+    private int antalGrön = 0;
+    private int antalGul = 0;
+    private int antalLila = 0;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void KlickRöst(object sender, RoutedEventArgs e)
+    {
+        if (sender == röd)
+        {
+            antalRöd++;
+        }
+        else if (sender == blå)
+        {
+            antalBlå++;
+        }
+        else if (sender == grön)
+        {
+            antalGrön++;
+        }
+        else if (sender == gul)
+        {
+            antalGul++;
+        }
+        else if (sender == lila)
+        {
+            antalLila++;
+        }
+
+        tbxResultat.Text = $"Röd: {antalRöd}, Blå: {antalBlå}, Grön: {antalGrön}, Gul: {antalGul}, Lila: {antalLila}";
+    }
+
+    private void KlickÅterställ(object sender, RoutedEventArgs e)
+    {
+        antalBlå = 0;
+        antalGrön = 0;
+        antalRöd = 0;
+        antalLila = 0;
+        antalGul = 0;
+
+        tbxResultat.Text = $"Röd: {antalRöd}, Blå: {antalBlå}, Grön: {antalGrön}, Gul: {antalGul}, Lila: {antalLila}";
     }
 }
